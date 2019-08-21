113057013a124db9b0c6f7c6db53f284142793c6
PrKo <pradip.kothawale@gmail.com>
2019-08-21 12:15:14 +0530

    Added new interesting pattern
          1
       1 2 1
    1 2 3 2 1

--- pattern.py
+++ pattern.py
@@ -135,3 +135,33 @@ PS C:\Projects\Python> python .\sample.py
  * * * * * * * * * * * * * * * * * * *
 PS C:\Projects\Python>
 '''
+def pattern(n):
+    for i in range(1,n+1):
+        k = n - i
+        for j in range(k):
+            print(end="  ")
+        for a in range(1,i+1):
+            print(a,end=" ")
+        for b in range(i,1,-1):
+            print(b-1,end=" ")
+        for j in range(k):
+            print(end="  ")
+        print()
+
+if __name__ == '__main__':
+    pattern(10)
+
+'''
+O/P:
+PS C:\Projects\Python> python .\sample.py
+                  1
+                1 2 1
+              1 2 3 2 1
+            1 2 3 4 3 2 1
+          1 2 3 4 5 4 3 2 1
+        1 2 3 4 5 6 5 4 3 2 1
+      1 2 3 4 5 6 7 6 5 4 3 2 1
+    1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
+  1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
+1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1
+'''
